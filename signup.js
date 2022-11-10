3513930fa22cd1b0ec70fad620d45efad8cb6014
aaka8566 <aakash8566@gmail.com>
2022-11-11 01:57:33 +0530

    p

--- signup.js
+++ signup.js
@@ -1,6 +1,121 @@
+let showtoknow=false;
+
+const getprofile=async(u,t)=>{
+    console.log(u,t,"aaaa");
+let res=await fetch(`https://masai-api-mocker.herokuapp.com/user/${u}`,{
+    
+    headers:{
+        Authorization: `Bearer ${t}`,
+        "Content-Type":"application/json"
+    }
+});
+let res1=await res.json();
+console.log(res1);
+let blackdiv=document.querySelector(".signupdiv");
+blackdiv.innerHTML=null;
+let p=document.createElement("h1");
+p.innerText=`Welcome ${res1.name}`;
+let img=document.createElement("img");
+    img.src="https://smallimg.pngkey.com/png/small/330-3307329_cross-thin-comments-cross-icon-black-png.png";
+    img.setAttribute("id","cross1");
+    img.onclick=()=>{
+        let blackdiv=document.querySelector(".signupdiv");
+        blackdiv.removeAttribute("id","showshow")
+        blackdiv.innerHTML=null;
+        showtoknow=true;
+    }
+blackdiv.append(p,img);
+}
+
+
+
+
+
+
+
+
+
+
+
+const loginstart=(u,p)=>{
+    console.log(u,p);
+    
+    class user{
+    constructor(){
+    
+    }
+    async login(u,p){
+        const logindata={
+            password:p,
+            username:u
+        };
+        try{
+        let response=await fetch(`https://masai-api-mocker.herokuapp.com/auth/login`,{
+            method:"POST",
+            body:JSON.stringify(logindata),
+            
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+//         const test1=response.headers.get('content-type')?.includes('application/json');
+// if(!response.ok) throw `Error! status: ${response.status}`
+
+console.log(response.status);
+        let res1=await response.json();
+        let b=res1.token;
+        console.log(res1.token);
+        getprofile(u,res1.token);
+    }
+    catch(err){
+        return ;
+    }
+    }
+    
+    
+    
+    }
+    let User=new user();
+    User.login(u,p);
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let show=document.getElementById("show");
 show.onclick=()=>{
-    showdiv();
+    if(showtoknow==false){showdiv();}
+    
 }
 const showdiv=()=>{
     let blackdiv=document.querySelector(".signupdiv");
@@ -133,8 +248,14 @@ lastbuttondiv.setAttribute("class","lastbuttondiv")
 let lastbutton=document.createElement("button");
 lastbutton.innerText="CONTINUE"
 lastbutton.setAttribute("class","lastbutton");
+
 lastbuttondiv.append(lastbutton);
 blackdiv.append(h1,formdiv,lastline,lastbuttondiv);
+
+
+
+//setting button disabled
+
 
 lastbutton.onclick=()=>{
 let form=document.querySelector(".formreg");
@@ -144,39 +265,162 @@ let username=form.username.value;
 let password=form.password.value;
 let mobile=form.mobile.value;
 let description=form.description.value;
-allformdata(name,email,username,password,mobile,description);
+let checkcheckbox=document.getElementById("checkbox").checked;
+if(name!=""&&email!=""&&username!=""&&password!=""&&mobile!=""&&description!=""&&checkcheckbox!=false){
+    
+    allformdata(name,email,username,password,mobile,description);
+}
+
+else{
+    alert("Fill All The Details")
+}
 }
 
 
 }
 
 
-const allformdata=(name,username,password,mobile,description)=>{
+//login all times
+let paswordimage=document.getElementById("passwordimage");
+paswordimage.onclick=()=>{
+let username=document.getElementById("id").value;
+let password=document.getElementById("password1").value;
+   loginstart(username,password);
+}
 
+//login all times
+
+
+
+
+const allformdata=(name,email,username,password,mobile,description)=>{
+// console.log(name,username,password,mobile,description);
 class user{
 constructor(){
 
 }
-validate(username){
-   return username.includes ("@")?false:true;
+validateusername(u){
+   return u.includes ("@")?false:true;
 }
-validate(password){
-    return password.length>=8?true:false;
+validatepassword(p){
+    return p.length>=8?true:false;
+}
+async signup(n,e,u,p,m,d){
+
+let isvalidated=this.validateusername(u)&&this.validatepassword(p);
+
+if(isvalidated)
+{
+    this.name=n;
+    this.email=e;
+    this.username=u;
+    this.password=p;
+    this.mobile=m;
+    this.description=d;
+}
+
+const res=await fetch(`https://masai-api-mocker.herokuapp.com/auth/register`,{
+    method:"POST",
+    body:JSON.stringify(this),
+    headers:{
+        "Content-Type":"application/json"
+    }
+});
+const res1=await res.json();
+console.log(res1.message);
+if(res1.message=="Registration Success"){
+    console.log("done");
+    let blackdiv=document.querySelector(".signupdiv");
+   let h1=document.createElement("h1");
+   h1.innerText="Registered Successfully"
+   blackdiv.innerHTML=null;
+   let nowcansignin=document.createElement("button");
+   nowcansignin.innerText="Continue To Sign In";
+   nowcansignin.setAttribute("class","nowcansignin");
+
+
+   nowcansignin.onclick=()=>{
+    blackdiv.innerHTML=null;
+   // appending after registration
+   let img1=document.createElement("img");
+   img1.setAttribute("class","cross");
+   img1.src="https://smallimg.pngkey.com/png/small/330-3307329_cross-thin-comments-cross-icon-black-png.png";
+
+   let divallfields=document.createElement("div");
+   divallfields.setAttribute("class","allfields");
+  let divinputforid=document.createElement("div");
+  divinputforid.setAttribute("id","inputforid");
+
+let div1=document.createElement("div");
+let inputid=document.createElement("input");
+inputid.setAttribute("placeholder","Apple id");
+inputid.setAttribute("type","text");
+inputid.setAttribute("id","id");
+let imginputforiddiv1=document.createElement("img");
+imginputforiddiv1.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS753-RxOsj-3vnG6wVbHxwAseHMAS8FxcffQ&usqp=CAU";
+div1.append(inputid,imginputforiddiv1);
+
+let div2=document.createElement("div");
+let inputpassword=document.createElement("input");
+inputpassword.setAttribute("placeholder","Password");
+inputpassword.setAttribute("type","passowrd");
+inputpassword.setAttribute("id","password1");
+let imginputforiddiv2=document.createElement("img");
+imginputforiddiv2.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS753-RxOsj-3vnG6wVbHxwAseHMAS8FxcffQ&usqp=CAU";
+imginputforiddiv2.onclick=()=>{
+   let username= inputid.value;
+   let password=inputpassword.value;
+   loginstart(username,password);
+}
+div2.append(inputpassword,imginputforiddiv2);
+
+divinputforid.append(div1,div2);
+
+  let img2=document.createElement("img");
+img2.src="https://cdn.mos.cms.futurecdn.net/6bTF6C2QiWXvhi33fJi3AC.jpg";
+let firstp=document.createElement("p");
+firstp.innerText="Sign in with your Apple ID";
+let secondp=document.createElement("p");
+secondp.innerText="You will be signed in to Apple TV and Apple Music";
+let but1=document.createElement("button");
+but1.innerText="Create New Apple ID";
+but1.setAttribute("id","createnewid");
+let but2=document.createElement("button");
+but2.innerText="Forgot ID or Password?";
+but2.setAttribute("id","forgotidps");
+
+
+divallfields.append(img2,firstp,secondp,divinputforid,but1,but2);
+
+blackdiv.append(img1,divallfields);
+
+    blackdiv.setAttribute("id","showshow");
+
+
+
+
+   }
+   blackdiv.append(h1,nowcansignin);
+   
+    
+}
+else if(res1.message=="Registration failed, user already exists"){
+    alert("User already exists ,Try with a diiferent ID")
+}
+else{
+    console.log("Something went wrong ,Retry");
+}
 }
 
 
 
+}
+
+let User=new user();
+User.signup(name,email,username,password,mobile,description);
 
 }
 
-
-
-
-
-
-
-
-}
 
 
 

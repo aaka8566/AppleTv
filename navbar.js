b87f194cb574c91915087850e91e9cec4811096d
arpit2444 <arpitjain2444@gmail.com>
2022-11-10 23:30:41 +0530

    day3 Arpit

---
+++ navbar.js
@@ -0,0 +1,25 @@
+const navbar = ()=>{
+    return `<img id="navimg" src="./New folder/logo.png" alt="logo">
+    <div>
+        <div id="navinput">
+            <i class="fa fa-search icon"></i>
+    <input type="text"  placeholder="Search" >
+</div>
+<div>
+ 
+    <button><img src="./New folder/icon_for_button.png" alt="">  Sign In</button>
+</div>
+</div>`
+}
+
+const footer = ()=>{
+    return `  <div id="footer1">
+    <p>Copyright © 2022 Planet Inc. All rights reserved.</p>
+</div>
+<div id="footer2">
+    <p>Internet Service Terms  &nbsp |  &nbsp  Planet Tv & Privacy  &nbsp |   &nbsp Cokkie Warning  &nbsp|  &nbsp  Support </p>
+</div>`
+}
+
+
+export { navbar , footer}

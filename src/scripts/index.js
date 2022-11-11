3ae0e3a477c88a37c879e1c76e562ddc8aaeabdb
pandeyneha0123 <pandeyneha0123>
2022-11-11 14:27:36 +0530

    add

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,11 @@
+import { navbar , footer } from "./navbar.js";
 
+let navdiv = document.getElementById("navbar");
+
+navdiv.innerHTML = navbar();
+
+let footdiv = document.getElementById("footer");
+footdiv.innerHTML= footer();
 
 
 
